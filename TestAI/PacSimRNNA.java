565d0e7f41e4b8c69317eb639895897ab1dc600b
Richard Snyder <richardwsnyder2016@gmail.com>
2018-09-07 08:35:26 -0400

    Some Changes

--- TestAI/PacSimRNNA.java
+++ TestAI/PacSimRNNA.java
@@ -1,4 +1,8 @@
-
+/*
+University of Central Florida
+CAP4630 - Fall 2018
+Authors: Richard Snyder, Jimmy Seeber
+*/
 import java.awt.Point;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,13 +15,14 @@ import pacsim.PacUtils;
 import pacsim.PacmanCell;
 
 /**
- * Simple RNNA Re-planning Search Agent
- * @author Dr. Demetrios Glinos
+ * RNNA Search Agent
+ * @author Richard Snyder and Jimmy Seeber
  */
 public class PacSimRNNA implements PacAction {
 
    private List<Point> path;
    private int simTime;
+   int solutionMoves;
 
    public PacSimRNNA( String fname ) {
       PacSim sim = new PacSim( fname );
@@ -25,7 +30,7 @@ public class PacSimRNNA implements PacAction {
    }
 
    public static void main( String[] args ) {
-      System.out.println("\nTSP using simple replanning agent by Dr. Demetrios Glinos:");
+      System.out.println("\nTSP using RNNA agent by Richard Snyder and Jimmy Seeber:");
       System.out.println("\nMaze : " + args[ 0 ] + "\n" );
       new PacSimRNNA( args[ 0 ] );
    }
@@ -34,15 +39,16 @@ public class PacSimRNNA implements PacAction {
    public void init() {
       simTime = 0;
       path = new ArrayList<>();
+      solutionMoves = 0;
    }
 
    @Override
    public PacFace action( Object state ) {
 
       PacCell[][] grid = (PacCell[][]) state;
-      PacmanCell pc = PacUtils.findPacman( grid );
+      PacmanCell pc = PacUtils.findPacman(grid);
 
-      // make sure Pac-Man is in this game
+      // check to see if there exists a PacMan in the game
       if( pc == null ) return null;
 
       // if current path completed (or just starting out),
@@ -50,13 +56,7 @@ public class PacSimRNNA implements PacAction {
       // measure and generate a path to that target
 
       if( path.isEmpty() ) {
-         Point tgt = PacUtils.nearestFood( pc.getLoc(), grid);
-         path = BFSPath.getPath(grid, pc.getLoc(), tgt);
-
-         System.out.println("Pac-Man currently at: [ " + pc.getLoc().x
-               + ", " + pc.getLoc().y + " ]");
-         System.out.println("Setting new target  : [ " + tgt.x
-               + ", " + tgt.y + " ]");
+         int[][] costTable = generateCostTable(grid, pc); 
       }
 
       // take the next step on the current path
@@ -66,5 +66,41 @@ public class PacSimRNNA implements PacAction {
       System.out.printf( "%5d : From [ %2d, %2d ] go %s%n",
             ++simTime, pc.getLoc().x, pc.getLoc().y, face );
       return face;
+   }
+
+
+   private int[][] generateCostTable(PacCell[][] g, PacmanCell p)
+   {
+      List<Point> food = PacUtils.findFood(g); 
+      int x, y, cost;
+
+      // table is size of food pellets + 1
+      int tbSize = food.size() + 1; 
+      int[][] costTable = new int[tbSize][tbSize]; 
+
+      for(x = 1; x < tbSize; x++)
+      {
+         cost = BFSPath.getPath(g, p.getLoc(), food.get(x - 1)).size(); 
+         costTable[0][x] = cost;
+         costTable[x][0] = cost;
+      }
+
+      for(x = 1; x < tbSize; x++)
+      {
+         for(y = 1; y < tbSize; y++)
+            costTable[x][y] = BFSPath.getPath(g, food.get(x - 1), food.get(y - 1)).size(); 
+      }
+
+      System.out.println("Cost Table:\n");
+      for(x = 0; x < tbSize; x++)
+      {
+         for(y = 0; y < tbSize; y++)
+            System.out.printf("%-3d", costTable[x][y]);
+         System.out.println(); 
+      }
+
+      System.out.println("\n"); 
+
+      return costTable; 
    }
 }
