f0ecefd42a266f5a2f4c36aa623ee6746e275625
Jimmy Seeber <jimlseeber@gmail.com>
2018-09-16 19:53:55 -0400

    branching

--- TestAI/PacSimRNNA.java
+++ TestAI/PacSimRNNA.java
@@ -66,7 +66,7 @@ public class PacSimRNNA implements PacAction {
       if( path.isEmpty() ) {
          int[][] costTable = generateCostTable(grid, pc);
          pellets = generateFoodTable(grid);
-         int[] orderOfFood = aSolPath(costTable, possiblePath, pellets);
+         int[] orderOfFood = searchAllPaths(costTable, possiblePath, pellets);
          allPaths.add(possiblePath);
          for(int j = 0; j < possiblePath.size(); j++)
          {
@@ -160,6 +160,135 @@ public class PacSimRNNA implements PacAction {
       return food;
    }
 
+
+
+
+   //Each time the main path branches, aSolPath will be called to termianlly
+   //execute the path and also return its totalCost. This method will
+   //govern all solution paths and return the overall least cost path.
+   private int[] searchAllPaths(int[][] costTable, List<Point> pathPoints, List<Point> foodTable){
+       int idx=0, i=0, j=0, testVal=0, testIdx=0, minTotalCost=0, step=0, row=0, col=0, count=0;
+       int n = costTable[0].length, branches=-1, min = Integer.MAX_VALUE, totalBranches=1;
+       int[] minVals = new int[n];
+       int[] minValIndexes = new int[n];
+       int[] validSearchSpace = new int[n*n];
+       ArrayList<int[]> solIndexes = new ArrayList<int[]>();
+       ArrayList<Integer> solIndexCandidate = new ArrayList<Integer>();
+       ArrayList<int[]> searchSpaceCloud = new ArrayList<int[]>();
+       int[] temp = new int[n];
+       int[] aNewSearchSpace = new int[n*n];
+       Boolean branch=false, end = false;
+
+       //Initiate the main branch:
+       // 0 1 1 1 1 1 1 1 1 1 1
+       // 0 0 1 1 1 1 1 1 1 1 1
+       // 0 1 0 1 1 1 1 1 1 1 1
+       // 0 1 1 0 1 1 1 1 1 1 1
+       // 0 1 1 1 0 1 1 1 1 1 1
+       // 0 1 1 1 1 0 1 1 1 1 1
+       // 0 1 1 1 1 1 0 1 1 1 1
+       // 0 1 1 1 1 1 1 0 1 1 1
+       // 0 1 1 1 1 1 1 1 0 1 1
+       // 0 1 1 1 1 1 1 1 1 0 1
+       // 0 1 1 1 1 1 1 1 1 1 0
+       validSearchSpace = initSearchSpace(validSearchSpace, n);
+       searchSpaceCloud.add(validSearchSpace);
+       // showSearchSpace(searchSpaceCloud.get(0), n);
+
+       //initialize main path and values:
+       // Indexes: 0 0 0 0 0 0 0 0 0 0
+       solIndexes.add(minValIndexes);
+       // System.out.print("\nIndexes: ");
+       // showArray(solIndexes.get(0));
+
+       // minVals: 0 2147483647 2147483647 2147483647 2147483647 2147483647 2147483647 2147483647 2147483647 2147483647 2147483647
+       // System.out.print("\nminVals: ");
+       minVals = initMinVals(minVals);
+       // showArray(minVals);
+       // System.out.println();
+
+       // solIndexes.get(0);
+
+       //Find all minimum paths:
+       for(idx=1; idx<4; idx++){
+           i=0;
+           System.out.println("-----------------------------------");
+           System.out.println("idx: " + idx + ") list length: " + solIndexes.size());
+           System.out.println("-----------------------------------");
+
+           //Populate the minimum cost sequences:
+           //solIndexes says how many lowest cost paths exist
+           while(i < solIndexes.size() ){
+               //traverse a row:
+               step = solIndexes.get(i)[idx-1]*n;
+               // showSearchSpace(searchSpaceCloud.get(i), n);
+               min = findMinVal(costTable, searchSpaceCloud.get(i), step);
+               branches = countBranches(costTable, searchSpaceCloud.get(i), step, min);
+               solIndexCandidate = findMinIndexes(costTable, searchSpaceCloud.get(i), step, min);
+               totalBranches += branches-1;
+
+               System.out.println("step: " + step + " i:" + i);
+               System.out.println("min: " + min);
+               System.out.print("branches " + branches + ": " + totalBranches  + "\nindex: ");
+               //taking the base array
+               temp = solIndexes.remove(i);
+               aNewSearchSpace = searchSpaceCloud.get(i);
+
+               j=0;
+               showArray(temp);
+
+               while(solIndexCandidate.size() >  0){
+                    //taking the new branching indexes and adding them to the base int[]
+                    temp[idx] = solIndexCandidate.remove(0);
+
+                    System.out.print(temp[idx] + " (" + j + ") ");
+                    solIndexes.add(j, temp);
+                    showArray(solIndexes.get(j));
+                    aNewSearchSpace = eliminateAll(solIndexes.get(j)[idx-1], searchSpaceCloud.get(j), n);
+                    searchSpaceCloud.add(j, eliminateAll(solIndexes.get(j)[idx-1], searchSpaceCloud.get(j), n) );
+                    showSearchSpace(aNewSearchSpace, n);
+                    System.out.println();
+                    j++;
+               }
+
+               //Show dynamic branching arrays:
+               // System.out.print("\n" + i + ") ");
+               // for(j=0; j<n; j++){
+               //     System.out.print(solIndexes.get(i)[j] + " ");
+               // }
+               // System.out.println("\n");
+               // showSearchSpace(searchSpaceCloud.get(i), n);
+               // System.out.println("\n\n");
+               i++;
+               // System.out.println(i)
+          }
+
+
+
+         //set terminating condition:
+         //solIndexes.remove(0);
+
+       }
+
+
+
+
+       solIndexes.add( aSolPath(costTable, pathPoints, foodTable) );
+       //return the first solution of minimal cost:
+       minValIndexes = solIndexes.remove(0);
+
+       return minValIndexes;
+   }
+
+
+
+
+
+
+
+
+
+
    private int[] aSolPath(int[][] costTable, List<Point> pathPoints, List<Point> foodTable){
      int idx=0, i=0, testVal=0, testIdx=0, minTotalCost=0, step=0, row=0, col=0, count=0;
      int n = costTable[0].length;
@@ -167,11 +296,15 @@ public class PacSimRNNA implements PacAction {
      int[] minValIndexes = new int[n];
      int[] validSearchSpace = new int[n*n];
      ArrayList<int[]> solIndexes = new ArrayList<int[]>();
+     ArrayList<int[]> searchSpaceCloud = new ArrayList<int[]>();
+     int[] aNewSetOfIndexes = new int[n];
+     int[] aNewSearchSpace = new int[n*n];
      int branches=0;
 
 
     //Call helper functions to perform initializations:
     validSearchSpace = initSearchSpace(validSearchSpace, n);
+    searchSpaceCloud.add(validSearchSpace);
     minVals = initMinVals(minVals);
     // showGrid(n);
 
@@ -185,7 +318,7 @@ public class PacSimRNNA implements PacAction {
         //Before searching the next row, finish end of row procedures
         if(count != 0 && count%n == 0){
             //Branch implementation: Re-evaluate the entire row with the least cost val
-            i=0;
+            i=0; branches = 0;
             while(i < n){
                 step--;
                 row = step/n;
@@ -193,17 +326,42 @@ public class PacSimRNNA implements PacAction {
 
                 //When re-traversing the row, log each branch in the arrayList
                 if(minVals[idx] == costTable[row][col] ){
-                    if(step%n == minValIndexes[idx]){
-                        ;
-                    }else{
+                    //A branch has been detected, add it and its searchSpace to the arrayLists
+                    if(step%n != minValIndexes[idx]){
                         branches++;
-                        System.out.println(step%n + " " + minValIndexes[idx] + ": " + branches);
+
+                        // //make a copy of the main branch
+                        // aNewSetOfIndexes = minValIndexes;
+                        // aNewSearchSpace = validSearchSpace;
+                        //
+                        // //Save the new direction of the branching path
+                        // aNewSetOfIndexes[idx] = step%n;
+                        //
+                        // //Refactor the arrayLists according to their respective paths
+                        // aNewSearchSpace = reduceSearchSpace(aNewSearchSpace, aNewSetOfIndexes, step%n, n);
+                        // aNewSearchSpace = eliminateAll(aNewSetOfIndexes, aNewSearchSpace);
+                        //
+                        // //store their solutions for retrieval, later
+                        // searchSpaceCloud.add(aNewSearchSpace);
+                        // solIndexes.add( aNewSetOfIndexes );
+                        // System.out.println(step%n + " " + minValIndexes[idx]);
                     }
                 }
                 i++;
             }
             step += n;
+
+
+            //concurrently reduce the search space for the cloud of low cost solutions and their
+            // for(i=0; i<searchSpaceCloud.size(); i++){
+            //     System.out.print(i);
+            //     showArray(aNewSetOfIndexes);
+            //     showSearchSpace(aNewSearchSpace, n);
+            //     searchSpaceCloud.set(i, reduceSearchSpace(aNewSearchSpace, aNewSetOfIndexes, idx, n) );
+            // }
+            System.out.println();
             validSearchSpace = reduceSearchSpace(validSearchSpace, minValIndexes, idx, n);
+
             //locate the next cell location to start traversing the row
             idx++;
             step = minValIndexes[idx-1]*n;
@@ -258,6 +416,16 @@ public class PacSimRNNA implements PacAction {
       }
       System.out.println();
 
+      // i=0;
+      // while(solIndexes.size() != 0 ){
+      //   aNewSetOfIndexes = solIndexes.remove(0);
+      //   for(i=0; i<n;i++){
+      //     System.out.print(aNewSetOfIndexes[i]);
+      //   }
+      //   System.out.println();
+      //   i++;
+      // }
+
       return minValIndexes;
 
   }
@@ -277,6 +445,70 @@ public class PacSimRNNA implements PacAction {
         }
         return validSearchSpace;
     }
+
+    int findMinVal(int[][] costTable, int [] validSearchSpace, int step){
+      int minVal=Integer.MAX_VALUE, n =costTable[0].length, row, col;
+      row = step/n;
+      col = step%n;
+      // System.out.println("step : " + step + " row: " + row + " col: " + col );
+      //Handle diagonal cases:
+      if( (costTable[row][col] != 0) && (validSearchSpace[step] == 1) ){
+        minVal = costTable[row][col];
+      }else{
+        minVal = costTable[row][col+1];
+      }
+      // System.out.println("\n" + minVal);
+      // System.out.print(step + ": " + costTable[row][col] );
+      for(int i=0; i<n-1; i++){
+          step++;
+          row = step/n;
+          col = step%n;
+          // System.out.print(" " + costTable[row][col]);
+          if(validSearchSpace[step] == 1){
+              if(minVal > costTable[row][col]){
+                minVal = costTable[row][col];
+              }
+          }
+      }
+      // System.out.println("\n" + minVal);
+      return minVal;
+    }
+
+    int countBranches(int[][] costTable, int [] validSearchSpace, int step, int min){
+      int branches=0, n =costTable[0].length, row, col;
+      for(int i=0; i<n-1; i++){
+          row = step/n;
+          col = step%n;
+          if(validSearchSpace[step] == 1){
+              if(min == costTable[row][col]){
+                branches++;
+              }
+          }
+          step++;
+      }
+      return branches;
+    }
+
+    //returns the column index value
+    ArrayList<Integer> findMinIndexes(int[][] costTable, int [] validSearchSpace, int step, int min){
+      int n =costTable[0].length, row, col;
+      ArrayList<Integer> branchIndexes = new ArrayList<Integer>();
+      row = step/n;
+      col = step%n;
+      for(int i=0; i<n-1; i++){
+          step++;
+          row = step/n;
+          col = step%n;
+          if(validSearchSpace[step] == 1){
+              if(min == costTable[row][col]){
+                branchIndexes.add(col);
+              }
+          }
+      }
+      return branchIndexes;
+    }
+
+
 
     void showSearchSpace(int[] validSearchSpace, int n){
       int i, j, step=0;
@@ -343,4 +575,22 @@ public class PacSimRNNA implements PacAction {
       // System.out.println("\n\n");
       return validSearchSpace;
     }
+
+    private static int[] eliminateAll(int index, int[] existingSearchSpace, int n)
+    {
+      int i, j, row=0, col=0, step=0;
+      step = index*n;
+      row = index;
+      // System.out.println("step: " + step);
+      for(i=0; i<n; i++){
+
+          // System.out.print("row: " + row + " col: " + (step+i) + "\n");
+          existingSearchSpace[row] = 0;
+          existingSearchSpace[step+i] = 0;
+          row += n;
+      }
+      // System.out.println();
+      return existingSearchSpace;
+    }
+
 }
